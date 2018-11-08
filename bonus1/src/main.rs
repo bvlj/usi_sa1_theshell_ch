3c43e6f17227ad713f50f0be2d66db06f395adef
maggicl <maggicl@usi.ch>
2018-11-08 20:18:09 +0000

    docs: Updated page creation guide for 'category-page' variable. bonus: bonus1 works for external links
    git-svn-id: svn+ssh://atelier.inf.usi.ch/home/bevilj/group-1@68 a672b425-5310-4d7a-af5c-997e18724b81

--- bonus1/src/main.rs
+++ bonus1/src/main.rs
@@ -39,7 +39,7 @@ fn find_urls(html: &str) -> Vec<String> {
     results
 }
 
-fn is_url_working(url: &str) -> bool {
+fn is_external_url_working(url: &str) -> bool {
     let res: Result<reqwest::Response, reqwest::Error> = reqwest::get(url);
     match res {
         Ok(r) => r.status().is_success(),
@@ -49,10 +49,10 @@ fn is_url_working(url: &str) -> bool {
 
 #[test]
 fn test_is_url_working() {
-    assert!(is_url_working("https://www.google.com"));
-    assert!(is_url_working("https://xkcd.com"));
-    assert!(!is_url_working("https://xkcd.com/404"));
-    assert!(!is_url_working("notaurl"));
+    assert!(is_external_url_working("https://www.google.com"));
+    assert!(is_external_url_working("https://xkcd.com"));
+    assert!(!is_external_url_working("https://xkcd.com/404"));
+    assert!(!is_external_url_working("notaurl"));
 }
 
 fn main() {
@@ -75,9 +75,19 @@ fn main() {
                     lazy_static! {
                         static ref RE: Regex = Regex::new(".html$").unwrap();
                     }
-                    
-                    if RE.is_match(en.file_name().to_str().unwrap()) {
-                        println!("{}", en.path().display());
+                   
+                    let file: &str = en.file_name().to_str().unwrap();
+                    if RE.is_match(file) {
+                        let path: &str = en.path().to_str().unwrap();
+                        let contents: String = read_file(path)
+                            .expect("cannot read");
+
+                        let urls: Vec<String> = find_urls(contents.as_str());
+                        for url in urls {
+                            if !is_external_url_working(&url) {
+                                println!("{}: '{}' is a broken link.", path, url);
+                            }
+                        }
                     }
                 }
                 Err(e) => {
