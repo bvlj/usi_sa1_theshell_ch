8f667b6463cf30171448ebc21f1e3f10320f27c2
annoum <annoum@usi.ch>
2018-11-13 15:06:16 +0000

    modify ab_re_quiz about fs with an example of quiz in javascript and added an example folder to do quiz in the other pages
    git-svn-id: svn+ssh://atelier.inf.usi.ch/home/bevilj/group-1@162 a672b425-5310-4d7a-af5c-997e18724b81

--- site/pages/fs/example_quiz_java/main.js
+++ site/pages/fs/example_quiz_java/main.js
@@ -3,43 +3,32 @@ function check(){
 	var question1 = document.quiz.question1.value;
 	var question2 = document.quiz.question2.value;
 	var question3 = document.quiz.question3.value;
-	var question4 = document.quiz.question4.value;
-	var question5 = document.quiz.question5.value;
-
-
 	var correct = 0;
 
 
-	if (question1 == "Is a location to a folder or file in a file system of a Operating System") {
+	if (question1 == "group1") {
 		correct++;
 }
-	if (question2 == "Is defined as specifying the location of a file or directory from the root directory(/)") {
+	if (question2 == "A type of..") {
 		correct++;
 }
-	if (question3 == "YES") {
+	if (question3 == "A type of..") {
 		correct++;
 	}
-	if (question4 == "") {
-		correct++;
-	}
-	if (question5 == "YES") {
-		correct++;
-	}
-
 
 	var pictures = ["img/win.gif", "img/meh.jpeg", "img/lose.gif"];
 	var messages = ["Great job!", "That's just okay", "You really need to do better"];
 	var score;
 
 	if (correct == 0) {
-		score = 4;
+		score = 2;
 	}
 
-	if (correct > 0 && correct < 5) {
-		score = 3;
+	if (correct > 0 && correct < 3) {
+		score = 1;
 	}
 
-	if (correct == 5) {
+	if (correct == 3) {
 		score = 0;
 	}
 
